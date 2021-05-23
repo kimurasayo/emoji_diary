fa5e9c276f12d1f0674fa79a3d3e4b07e41b1447
kimurasayo <otaemiiin@gmail.com>
2021-05-23 15:16:24 +0900

    Add: コードの補足説明を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tops#home'
-  
-  #new_user_path, users_path
-  resources :users, only: %i[new create]
-  
-  #login_path
+
+  # new_user_path, users_path
+  resources :users, only: [:new, :create]
+
+  # login_path
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
-  #logout_path
+  # logout_path
   delete 'logout', to: 'user_sessions#destroy', as: :logout
 end
