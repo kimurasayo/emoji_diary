93050f1aac3328f0eb2bc830f2f3763657ca129d
kimurasayo <otaemiiin@gmail.com>
2021-05-23 15:11:26 +0900

    Add: ログイン、ログアウト時のフラッシュメッセージを設置

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,21 +1,22 @@
 class UserSessionsController < ApplicationController
-  
   def new; end
 
-  #ログイン時はnameとpasswordの情報でDBからユーザーを探す
-  #もしユーザーが探せたらトップページに遷移、一致しなかったらログインページに遷移
+  # ログイン時はnameとpasswordの情報でDBからユーザーを探す
+  # もしユーザーが探せたらトップページに遷移、一致しなかったらログインページに遷移
   def create
     @user = login(params[:name], params[:password])
     if @user
-      redirect_back_or_to root_path
+      redirect_back_or_to root_path, success: t('.success')
     else
+      flash.now[:danger] = t('.fail')
       render :new
     end
   end
 
-  #ログアウト、トップページに遷移する
+  # ログアウト、トップページに遷移する
   def destroy
     logout
+    flash[:success] = t('.success')
     redirect_to root_path
   end
 end
