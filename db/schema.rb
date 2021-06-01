434eaa6f1e8ff5faebef9bc0b502d815d2079779
kimurasayo <otaemiiin@gmail.com>
2021-06-01 21:52:27 +0900

    Add: relationshipsテーブルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_044149) do
+ActiveRecord::Schema.define(version: 2021_06_01_055359) do
 
   create_table "diaries", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "feeling", null: false
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2021_05_27_044149) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
+
+  create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follower_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["user_id", "follower_id"], name: "index_relationships_on_user_id_and_follower_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -33,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_05_27_044149) do
   end
 
   add_foreign_key "diaries", "users"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
