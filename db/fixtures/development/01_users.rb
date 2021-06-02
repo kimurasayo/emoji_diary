dd8fe47ed3e8ac9fca79ae896d96cad938fb9668
kimurasayo <otaemiiin@gmail.com>
2021-06-02 18:11:31 +0900

    Add: ユーザーの初期データを作成

---
+++ db/fixtures/development/01_users.rb
@@ -0,0 +1,60 @@
+User.seed do |s|
+  s.id = 1
+  s.nickname = 'sayo'
+  s.name = 'kimsayo'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 2
+  s.nickname = 'eri'
+  s.name = 'eri1112'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 3
+  s.nickname = 'yuka'
+  s.name = 'yuka911'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 4
+  s.nickname = 'ayaka'
+  s.name = '110aya'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 5
+  s.nickname = 'asa'
+  s.name = 'asako'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 6
+  s.nickname = 'yuki'
+  s.name = 'bu8yuki'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 7
+  s.nickname = 'rina'
+  s.name = 'rina93'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 8
+  s.nickname = 'yuna'
+  s.name = 'yunana'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 9
+  s.nickname = 'nami'
+  s.name = 'namihe'
+  s.password  = "password"
+end
+User.seed do |s|
+  s.id = 10
+  s.nickname = 'mari'
+  s.name = 'maririn'
+  s.password  = "password"
+end
