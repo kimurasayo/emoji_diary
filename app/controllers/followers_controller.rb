0f26bde827b2e81ea7b5de2d72ad793e26179160
kimurasayo <otaemiiin@gmail.com>
2021-06-07 14:44:40 +0900

    Add: indexアクションを修正

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,7 +1,6 @@
 class FollowersController < ApplicationController
   # user_followers_path
   def index
-    @user = current_user
-    @users = @user.followings
+    @users = current_user.followings
   end
 end
