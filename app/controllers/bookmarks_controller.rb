967fe1cc88dead108c23085276e8799234605418
kimurasayo <otaemiiin@gmail.com>
2021-06-17 12:47:39 +0900

    Fix: rubocopの指摘による変更

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,14 +3,14 @@ class BookmarksController < ApplicationController
   def create
     @diary = Diary.find(params[:diary_id])
     current_user.bookmark(@diary)
-    #redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
   # ブックマークを外す
   def destroy
     @diary = current_user.bookmarks.find(params[:id]).diary
     current_user.unbookmark(@diary)
-    #redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
   end
 
   # 日記にブックマークしているユーザー一覧のページ
