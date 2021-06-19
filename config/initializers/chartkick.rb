e4ce63c86862c49157880be3b3c9e9770af946dd
kimurasayo <otaemiiin@gmail.com>
2021-06-19 17:08:54 +0900

    Add: 感情の変化を表したグラフをchartkickとHighchartsを使って実装

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,19 @@
+Chartkick.options = {
+  colors: ["#f5cdbd"],
+  message: { empty: "no diary on this month" },
+  suffix: "%",
+  min: 0,
+  max: 100,
+  library: {
+    title: {
+      text: 'recent emotional chages',
+      align: 'center',
+      style: {
+        color: '#c76e54'
+      }
+    },
+    tooltip: {
+      pointFormat: ''
+    }
+  }
+}
