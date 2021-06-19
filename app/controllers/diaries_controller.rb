a8e4c91c6499a6c18d36185521b2836dbe83516c
kimurasayo <otaemiiin@gmail.com>
2021-06-19 17:11:37 +0900

    Add: 顔文字に応じて点数をつけるメソッドを追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -20,6 +20,7 @@ class DiariesController < ApplicationController
   # 日記作成アクション。作成できたら一覧ページ、失敗したら日記新規作成ページへ。
   def create
     @diary = current_user.diaries.new(diary_params)
+    score_feeling
     if @diary.save
       redirect_to user_diaries_path(current_user)
     else
@@ -42,6 +43,7 @@ class DiariesController < ApplicationController
   # 日記更新アクション。作成できたら一覧ページ、失敗したら日記編集ページへ。
   def update
     @diary = current_user.diaries.find(params[:id])
+    score_feeling
     if @diary.update(diary_params)
       redirect_to user_diary_path(current_user)
     else
@@ -54,5 +56,30 @@ class DiariesController < ApplicationController
   # フォームから受け取ることのできる情報カラム
   def diary_params
     params.require(:diary).permit(:feeling, :body, :start_time)
+  end
+
+  def score_feeling
+    case @diary.feeling
+    when "🤩", "🥰", "😍", "😘", "🥳", "🤪"
+      @diary.score = 100
+    when "😋", "😜", "😝", "🤗", "😳", "🤣", "😂"
+      @diary.score = 90
+    when "😁", "😆", "😊", "😇", "🤤", "😎", "😚"
+      @diary.score = 80
+    when "😅", "😉", "😃", "😙", "😄", "🤑"
+      @diary.score = 70
+    when "😌", "🤓", "😏", "😛", "😗", "😀", "🙂", "🙃"
+      @diary.score = 60
+    when "😐", "😑", "😶", "😒", "🙄", "😬", "😲", "🥺", "🤐", "🤔"
+      @diary.score = 50
+    when "🤭", "🤫", "🤨", "🤥", "🧐", "😕", "😟", "🙁", "😦", "😧"
+      @diary.score = 40
+    when "😤", "😠", "😪", "😴", "😨", "😰", "😢", "😥", "😞", "😓", "😔"
+      @diary.score = 30
+    when "😷", "🤕", "🤧", "🥵", "🥶", "🥴", "😵", "😡", "😣", "🤯", "😩"
+      @diary.score = 20
+    when "😭", "😱", "😖", "😫", "🤬", "😈", "👿", "🤢", "🤮"
+      @diary.score = 10
+    end
   end
 end
