83c0cde59a75c57454e88fb66bd46fec31339bd8
kimurasayo <otaemiiin@gmail.com>
2021-06-29 20:55:02 +0900

    Add: Usersテーブルにroleカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_150403) do
+ActiveRecord::Schema.define(version: 2021_06_29_035113) do
 
   create_table "bookmarks", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_150403) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
