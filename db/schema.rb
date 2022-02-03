3cde10d6b311a668e92b4702c46b721f0b1d8c2a
kimurasayo <otaemiiin@gmail.com>
2022-02-03 16:02:54 +0900

    Fix: 使用していないLineBotの記述消去

--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,6 @@ ActiveRecord::Schema.define(version: 2021_07_17_131106) do
     t.index ["user_id"], name: "index_diaries_on_user_id"
   end
 
-  create_table "line_users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "uid"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "relationships", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "follower_id"
